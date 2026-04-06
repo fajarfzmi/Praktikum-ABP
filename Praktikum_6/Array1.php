d179cb1800598387cfacc2fbdd3d7771d4b6f573
fajarfzmi <161665789+fajarfzmi@users.noreply.github.com>
2026-04-06 20:05:21 +0700

    Add files via upload

---
+++ Praktikum_6/Array1.php
@@ -0,0 +1,14 @@
+<?php 
+$arrKendaraan = ["Mobil", "Pesawat", "Kereta Api", "Kapal Laut"]; 
+echo $arrKendaraan[0] . "<br>"; //Mobil 
+echo $arrKendaraan[2] . "<br>"; //Kereta Api 
+$arrKota = []; 
+$arrKota[] = "Jakarta"; 
+$arrKota[] = "Medan"; 
+$arrKota[] = "Bandung"; 
+$arrKota[] = "Malang"; 
+$arrKota[] = "Sulawesi"; 
+echo $arrKota[1] . "<br>"; //Medan  
+echo $arrKota[2] . "<br>"; //Bandung  
+echo $arrKota[4] . "<br>"; //Sulawesi 
+?> 
